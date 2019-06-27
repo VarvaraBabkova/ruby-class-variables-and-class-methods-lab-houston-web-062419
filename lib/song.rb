bc029b1fe0e0b848442b197050477ca9f459adb6
Varvara Babkova <varvara.munday@gmail.com>
2019-06-27 16:01:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song
 
+  attr_accessor :name, :artist, :genre
+
   @@artists = []
   @@count = 0
   @@genres = []
 
   def initialize(name, artist, genre)
+
+    @name = name
+    @genre = genre
+    @artist = artist
+    
     @@artists << artist
     @@artists.uniq!
 
