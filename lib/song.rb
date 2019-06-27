0e223815bc812aff661e8b59b8e37b8b22779c39
Varvara Babkova <varvara.munday@gmail.com>
2019-06-27 15:59:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,18 @@ class Song
 
   @@artists = []
   @@count = 0
+  @@genres = []
 
   def initialize(name, artist, genre)
     @@artists << artist
+    @@artists.uniq!
+
     @count += 1
+
+    @@genres << genre
+    @@genres.uniq!
+
+    
   end
 
 end
